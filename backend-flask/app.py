952e3b31a0b17c20ab815068f42003ef1afc3550
SANCHITMAHAJN <SANCHITMAHAJAN876@GMAIL.COM>
2023-03-04 02:32:18 +0000

    xray segments

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,7 +29,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 ## AWS XRAY ------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -40,6 +40,9 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+# XRAY --
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
